b6d1c14b2dd67edb59a5744e1acf5d3bbbaacd90
HardRope <61661973+HardRope@users.noreply.github.com>
2022-05-10 19:18:45 +0300

    Rename variables, discard unneccecary 'While'

--- telegram-publisher.py
+++ telegram-publisher.py
@@ -8,11 +8,11 @@ from dotenv import load_dotenv
 def get_paths(dir):
     dirpath = Path.cwd() / dir
     tree = os.walk(dirpath)
-    images = []
+    image_paths = []
     for filepaths, dirs, files in tree:
         for filename in files:
-            images.append(os.path.join(filepaths, filename))
-    return images
+            image_paths.append(os.path.join(filepaths, filename))
+    return image_paths
 
 
 if __name__ == '__main__':
@@ -22,11 +22,11 @@ if __name__ == '__main__':
     bot = telegram.Bot(token=tg_token)
     channel_id = os.getenv('CHANNEL-ID')
 
-    images_paths = get_paths('image')
+    image_paths = get_paths('image')
     time_delay = int(os.getenv('TIME-DELAY', default=86400))
 
-    while True:
-        for image in images_paths:
-            with open(image, "rb") as photo:
-              bot.send_document(chat_id=channel_id, document=photo)
-            time.sleep(time_delay)
+    for image in image_paths:
+        with open(image, "rb") as photo:
+            bot.send_document(chat_id=channel_id, document=photo)
+        time.sleep(time_delay)
+
