a163f6882d7f61d9ca1b1cf561fd884d365ef9b4
HardRope <61661973+HardRope@users.noreply.github.com>
2022-05-17 06:50:43 +0300

    correct function name

--- telegram-publisher.py
+++ telegram-publisher.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import telegram
 from dotenv import load_dotenv
 
-def get_paths(dir):
+def get_images_paths(dir):
     dirpath = Path.cwd() / dir
     tree = os.walk(dirpath)
     image_paths = []
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     bot = telegram.Bot(token=tg_token)
     channel_id = os.getenv('CHANNEL-ID')
 
-    image_paths = get_paths('image')
+    image_paths = get_images_paths('image')
     time_delay = int(os.getenv('TIME-DELAY', default=86400))
 
     for image in image_paths:
